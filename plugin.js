c25489a90ee6aa6cd8c3635a42f5d10a63e72b84
LogicZtech <59212629+LogicZtech@users.noreply.github.com>
2023-06-30 03:41:41 -0400

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,12 +1,12 @@
 // This plugin pings a list of roles when a modmail ticket goes unanswered for X amount of time.
 
 module.exports = function({ bot, knex, config, commands }) {
-const INTERVAL_IN_MINUTES = 15; // How often to check for unanswered tickets
-const UNRESPONDED_TICKETS_ROLES = ['role1', 'role2', 'role3']; // The roles to ping when a ticket goes unanswered
+const INTERVAL_IN_MINUTES = 60; // How often to check for unanswered tickets
+const UNRESPONDED_TICKETS_ROLES = ['Helper', 'Moderator']; // The roles to ping when a ticket goes unanswered
 
 // This function is called every INTERVAL_IN_MINUTES minutes
 function checkForUnrespondedTickets() {
-const tickets = knex('tickets').where('last_message_at', '<', Date.now() - INTERVAL_IN_MINUTES * 60000);
+const tickets = knex('tickets').where('last_message_at', '<', Date.now() - INTERVAL_IN_MINUTES * 60 * 60);
 
 tickets.each((ticket) => {
   UNRESPONDED_TICKETS_ROLES.forEach((role) => {
@@ -19,5 +19,5 @@ tickets.each((ticket) => {
 }
 
 // Start the timer to check for unanswered tickets
-setInterval(checkForUnrespondedTickets, INTERVAL_IN_MINUTES * 60000);
+setInterval(checkForUnrespondedTickets, INTERVAL_IN_MINUTES * 60 * 60);
 };
