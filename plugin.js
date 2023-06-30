edf6aa259adc70979b043598279ea7a28f6b2cfa
LogicZtech <59212629+LogicZtech@users.noreply.github.com>
2023-06-30 04:33:40 -0400

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,26 +1,25 @@
-// This plugin pings a list of roles when a modmail ticket goes unanswered for X amount of time.
+// This plugin pings a list of roles when a channel goes unanswered for X amount of time.
 
 module.exports = function({ bot, knex, config, commands }) {
-const INTERVAL_IN_MINUTES = 60; // How often to check for unanswered tickets
-const UNRESPONDED_TICKETS_ROLES = ['Helper', 'Moderator']; // The roles to ping when a ticket goes unanswered
+const INTERVAL_IN_MINUTES = 10; // How often to check for unanswered channels
+const UNRESPONDED_TICKETS_ROLES = ['@Helper']; // The roles to ping when a channel goes unanswered
 
 // This function is called every INTERVAL_IN_MINUTES minutes
-function checkForUnrespondedTickets() {
-const tickets = knex('tickets').where('last_message_at', '<', Date.now() - INTERVAL_IN_MINUTES * 60 * 60);
+function checkForUnansweredChannels() {
+const channels = knex('channels').where('last_message_at', '<', Date.now() - INTERVAL_IN_MINUTES * 60);
 
-tickets.each((ticket) => {
+channels.each((channel) => {
+if (channel.last_message_at < Date.now() - 60 * 60) {
 UNRESPONDED_TICKETS_ROLES.forEach((role) => {
 bot.users.inRoles(role).forEach((user) => {
-bot.chat.sendMessage(user, A modmail ticket has gone unresponded for ${INTERVAL_IN_MINUTES} minutes: ${ticket.id});
+bot.chat.sendMessage(user, This ticket has had NO activity in 1 hour. Please Review!, { channel });
 });
 });
+}
 });
 
 }
 
-// Start the timer to check for unanswered tickets
-setInterval(checkForUnrespondedTickets, INTERVAL_IN_MINUTES * 60 * 60);
-
-// Update the command property
-const command = ["m!ticket", "m!t"];
+// Start the timer to check for unanswered channels
+setInterval(checkForUnansweredChannels, INTERVAL_IN_MINUTES * 60);
 };
