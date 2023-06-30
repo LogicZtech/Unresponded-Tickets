fbc16cbd7555361719aac9720ec8a6c5ab65e4bc
LogicZtech <59212629+LogicZtech@users.noreply.github.com>
2023-06-30 07:18:11 -0400

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,25 +1,38 @@
-// This plugin pings a list of roles when a channel goes unanswered for X amount of time.
+// MasterZ - Modmail Plugin Pings X Roles when a channel goes unanswered for X Time
 
 module.exports = function({ bot, knex, config, commands }) {
-const INTERVAL_IN_MINUTES = 10; // How often to check for unanswered channels
-const UNRESPONDED_TICKETS_ROLES = ['@Helper']; // The roles to ping when a channel goes unanswered
+  const CHECK_INTERVAL = 10; // How often to check for unanswered channels
 
-// This function is called every INTERVAL_IN_MINUTES minutes
-function checkForUnansweredChannels() {
-const channels = knex('channels').where('last_message_at', '<', Date.now() - INTERVAL_IN_MINUTES * 60);
+  function checkForUnansweredChannels() {
+    const channels = bot.channels.cache.get('CHANNEL_ID');
+    const excludedChannels = [
+      1119349592467722321,
+      1090629350929481826,
+      1090629350929481825,
+      1124172328822124594,
+      1090629351269216278,
+      1090629547403255918,
+      1090629350929481819,
+      1090629350447120428,
+      1124130118168760361,
+      1113447304813351052,
+      1124085800104116385,
+    ];
 
-channels.each((channel) => {
-if (channel.last_message_at < Date.now() - 60 * 60) {
-UNRESPONDED_TICKETS_ROLES.forEach((role) => {
-bot.users.inRoles(role).forEach((user) => {
-bot.chat.sendMessage(user, This ticket has had NO activity in 1 hour. Please Review!, { channel });
-});
-});
-}
-});
+    channels.forEach((channel) => {
+      if (channel.lastMessageAt < Date.now() - 60 * 60 && !excludedChannels.includes(channel.id)) {
+        // The channel has not had any activity in the past 1 hour and is not excluded
+        const ROLES = ['@Helper'];
 
-}
+        ROLES.forEach((role) => {
+          bot.channels.fetch(role.id).then((helperChannel) => {
+            helperChannel.send(`This ticket has had NO activity in 1 hour. Please Review!`);
+          });
+        });
+      }
+    });
+  }
 
-// Start the timer to check for unanswered channels
-setInterval(checkForUnansweredChannels, INTERVAL_IN_MINUTES * 60);
+  // Start the timer to check for unanswered channels
+  setInterval(checkForUnansweredChannels, CHECK_INTERVAL * 60);
 };
