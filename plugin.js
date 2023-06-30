9d8aa53087b06fcbc4f5ff602815e58ad497264e
LogicZtech <59212629+LogicZtech@users.noreply.github.com>
2023-06-30 04:18:22 -0400

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -9,15 +9,18 @@ function checkForUnrespondedTickets() {
 const tickets = knex('tickets').where('last_message_at', '<', Date.now() - INTERVAL_IN_MINUTES * 60 * 60);
 
 tickets.each((ticket) => {
-  UNRESPONDED_TICKETS_ROLES.forEach((role) => {
-    bot.users.inRoles(role).forEach((user) => {
-      bot.chat.sendMessage(user, `A modmail ticket has gone unresponded for ${INTERVAL_IN_MINUTES} minutes: ${ticket.id}`);
-    });
-  });
+UNRESPONDED_TICKETS_ROLES.forEach((role) => {
+bot.users.inRoles(role).forEach((user) => {
+bot.chat.sendMessage(user, A modmail ticket has gone unresponded for ${INTERVAL_IN_MINUTES} minutes: ${ticket.id});
+});
+});
 });
 
 }
 
 // Start the timer to check for unanswered tickets
 setInterval(checkForUnrespondedTickets, INTERVAL_IN_MINUTES * 60 * 60);
+
+// Update the command property
+const command = ["m!ticket", "m!t"];
 };
